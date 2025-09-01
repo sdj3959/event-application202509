be7863fed7794445e6837bcedc8b13cdd00d976b
sdj3959 <dj9099@naver.com>
2025-09-01 14:21:51 +0900

    [FE] 이벤트 생성 기능

--- frontend/src/components/EventForm.jsx
+++ frontend/src/components/EventForm.jsx
@@ -1,10 +1,49 @@
 import styles from './EventForm.module.scss';
+import {useNavigate} from 'react-router-dom';
 
 const EventForm = () => {
+
+  // 새로고침 없이 페이지 이동
+  const navigate = useNavigate();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    // form에 입력한 값 가져오기
+    const formData = new FormData(e.target);
+
+    // 서버로 보낼 payload
+    const payload = {
+      title: formData.get('title'),
+      desc: formData.get('description'),
+      beginDate: formData.get('date'),
+      imageUrl: formData.get('image')
+    };
+    // console.log(payload);
+
+    // 서버에 POST 요청
+    (async () => {
+      const response = await fetch('http://localhost:9000/api/events', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+      });
+
+      if (response.ok) {
+        // 목록 페이지로 리다이렉트
+        navigate('/events');
+      }
+
+    })();
+  };
+
   return (
     <form
       className={styles.form}
-      noValidate>
+      noValidate
+      onSubmit={handleSubmit}
+    >
       <p>
         <label htmlFor='title'>Title</label>
         <input
