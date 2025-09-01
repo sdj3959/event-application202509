5084c2ca97ce4e18e231db2a264a0f10b2dd1619
sdj3959 <dj9099@naver.com>
2025-09-01 16:42:20 +0900

    [FE] 이벤트 삭제 버튼 기능

--- frontend/src/components/EventItem.jsx
+++ frontend/src/components/EventItem.jsx
@@ -1,12 +1,34 @@
 import styles from './EventItem.module.scss';
+import {useNavigate} from "react-router-dom";
 
 const EventItem = ({ event }) => {
+
+  const navigate = useNavigate();
+
   const {
+    id,
     title,
     desc: description,
     'img-url': image,
     'start-date': date,
   } = event;
+
+  const deleteHandler = e => {
+    e.preventDefault();
+
+    // 서버에 POST 요청
+    (async () => {
+      const response = await fetch(`http://localhost:9000/api/events/${id}`, {
+        method: 'DELETE',
+      });
+
+      if (response.ok) {
+        // 목록 페이지로 리다이렉트
+        navigate('/events');
+      }
+
+    })();
+  }
 
   return (
     <article className={styles.event}>
@@ -19,7 +41,7 @@ const EventItem = ({ event }) => {
       <p>{description}</p>
       <menu className={styles.actions}>
         <a href='#'>Edit</a>
-        <button>Delete</button>
+        <button onClick={deleteHandler}>Delete</button>
       </menu>
     </article>
   );
