9f27e2dc0a6900668e4b3b7fcd6de5001852b115
sdj3959 <dj9099@naver.com>
2025-09-03 09:52:39 +0900

    [BE] 이메일 인증메일 발송 로직

--- backend/src/main/java/com/study/event/service/EventUserService.java
+++ backend/src/main/java/com/study/event/service/EventUserService.java
@@ -2,8 +2,12 @@ package com.study.event.service;
 
 import com.study.event.domain.entity.EventUser;
 import com.study.event.repository.EventUserRepository;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,20 +17,71 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class EventUserService {
 
+    // 메일 발송인의 정보
+    @Value("${spring.mail.username}")
+    private String mailHost;
+
+    // 이메일 발송을 위한 의존객체
+    private final JavaMailSender mailSender;
+
     private final EventUserRepository eventUserRepository;
 
     // 이메일 중복확인 처리
+    @Transactional(readOnly = true)
     public boolean checkEmailDuplicate(String email) {
-        // 임시로 회원가입 진행
-        EventUser eventUser = EventUser.builder()
-                .email("test@test.com" + (int) (Math.random() * 99999))
-                .build();
-        eventUserRepository.save(eventUser);
 
         // 중복확인
         boolean flag = eventUserRepository.existsByEmail(email);
         log.info("Checking email {} is duplicate: {}", email, flag);
+
+        // 사용가능한 이메일인 경우 인증메일 발송
+        if (!flag) sendVerificationEmail(email);
+
         return flag;
+    }
+
+    // 이메일 인증코드 발송 로직
+    private void sendVerificationEmail(String email) {
+
+        // 인증코드 생성
+        String code = generateCode();
+
+        // 메일 전송 로직
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper messageHelper
+                    = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+
+            // 누구에게 이메일을 보낼지
+            messageHelper.setTo(email);
+
+            // 누가 보내는 건지
+            messageHelper.setFrom(mailHost);
+
+            // 이메일 제목 설정
+            messageHelper.setSubject("[인증메일] 중앙정보스터디 가입 인증 메일입니다.");
+            // 이메일 내용 설정
+            messageHelper.setText(
+                    "인증 코드: <b style=\"font-weight: 700; letter-spacing: 5px; font-size: 30px;\">" + code + "</b>"
+                    , true
+            );
+
+            // 메일 보내기
+            mailSender.send(mimeMessage);
+
+            log.info("{} 님에게 이메일이 발송되었습니다.", email);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("메일 발송에 실패했습니다.");
+        }
+
+    }
+
+    // 무작위로 1000~9999 사이의 랜덤 숫자를 생성
+    private String generateCode() {
+        return String.valueOf((int) (Math.random() * 9000) + 1000);
     }
 }
 
