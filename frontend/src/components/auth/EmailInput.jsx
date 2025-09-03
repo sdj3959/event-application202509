4a6e67ed2465a2861760c065cab0cd5208c9a24c
sdj3959 <dj9099@naver.com>
2025-09-03 10:54:09 +0900

    [FE] 이메일 패턴 검증

--- frontend/src/components/auth/EmailInput.jsx
+++ frontend/src/components/auth/EmailInput.jsx
@@ -1,12 +1,43 @@
+import {useEffect, useRef, useState} from "react";
+import styles from './SignUpForm.module.scss';
 
 const EmailInput = () => {
+
+  const emailRef = useRef();
+
+  // 에러 상태메시지를 관리
+  const [error, setError] = useState('');
+
+  // 화면이 렌더링 되자마자 입력창에 포커싱
+  useEffect(() => {
+    emailRef.current.focus();
+  }, []);
+
+  // 이메일 입력 이벤트
+  const hanldeEmail = (e) => {
+    const inputValue = e.target.value;
+
+    // 이메일 패턴 검증
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // 간단한 이메일 패턴 검사
+    if (!emailPattern.test(inputValue)) {
+      setError('이메일 형식이 올바르지 않습니다.');
+      return;
+    }
+
+    setError('');
+  };
+
   return (
     <>
       <p>Step 1: 유효한 이메일을 입력해주세요.</p>
       <input
+        ref={emailRef}
+        className={error ? styles.invalidInput : ''}
         type='email'
         placeholder='Enter your email'
+        onChange={hanldeEmail}
       />
+      {error && <p className={styles.errorMessage}>{error}</p>}
     </>
   );
 };
